be70f654e3d616f983f1fcfc1febf799b74af36d
Fathony Luthfillah <fathony@smart-leaders.net>
2022-10-05 19:04:28 +0700

    run everywhere

--- python/webserver.py
+++ python/webserver.py
@@ -1,8 +1,15 @@
+import os
 from bottle import route, run, static_file
+
+# cwd = os.getcwd()
+cwd = os.path.dirname(os.path.realpath(__file__))
+print(cwd)
+static_dir = os.path.join(cwd, 'static')
+print('static_dir:', static_dir)
 
 @route('/')
 def root():
-    return static_file('index.html', root='./static')
+    return static_file('index.html', root=static_dir)
 
 
 @route('/hello')
@@ -11,7 +18,7 @@ def hello():
 
 @route('/<filename:path>')
 def static(filename):
-    return static_file(filename, root='./static')
+    return static_file(filename, root=static_dir)
 
 
 run(host='localhost', port=8081, debug=True)
